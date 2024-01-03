4e995dd02ae79cac601a48adcd77e5521fc75810
Tayo Olowu <olowu.otayo@gmail.com>
2024-01-03 23:31:52 +0100

    nother update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,9 @@
 def uppercase(str):
     shift = ord('A')-ord('a')
     for ch in str:
-        upp = chr(ord(ch) + shift)
+        if ((ord(ch) >= ord('a')) and (ord(ch) <= ord('z'))):
+            upp = chr(ord(ch) + shift)
+        else:
+            upp = ch
         print("{}".format(upp), end="")
     print("", end="\n")
