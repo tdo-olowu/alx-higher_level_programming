14cd88405cc1af35363df229b6cb4ee879cbd1f7
Tayo Olowu <olowu.otayo@gmail.com>
2024-01-04 23:18:37 +0100

    updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-apb = add(a, b)
-print("{} + {} = {}".format(a, b, apb))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    apb = add(a, b)
+    print("{} + {} = {}".format(a, b, apb))
