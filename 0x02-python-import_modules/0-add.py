8fab44b6aa05ef6364a055b97960bea54d3ed9ce
Tayo Olowu <olowu.otayo@gmail.com>
2024-01-04 22:28:27 +0100

    updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a = 1
 b = 2
-apb = add_0.add(a, b)
+apb = add(a, b)
 print("{} + {} = {}".format(a, b, apb))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import sys
+av = sys.argv[:]
+argc = len(av) - 1
+msg = "arguments"
+punct = ":"
+if (argc == 1):
+    msg = msg[:-1]
+if (argc == 0):
+    punct = "."
+print("{} {}{}".format(argc, msg, punct))
+if (argc > 0):
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, av[i]))
