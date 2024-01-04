2621f5b53617df6b7dbfc29ebf6dd3dc1ca1ba3f
Tayo Olowu <olowu.otayo@gmail.com>
2024-01-04 23:30:10 +0100

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-av = sys.argv[:]
-argc = len(av) - 1
-msg = "arguments"
-punct = ":"
-if (argc == 1):
-    msg = msg[:-1]
-if (argc == 0):
-    punct = "."
-print("{} {}{}".format(argc, msg, punct))
-if (argc > 0):
-    for i in range(1, argc + 1):
-        print("{}: {}".format(i, av[i]))
+if __name__ == "__main__":
+    import sys
+    av = sys.argv[:]
+    argc = len(av) - 1
+    msg = "arguments"
+    punct = ":"
+    if (argc == 1):
+        msg = msg[:-1]
+    if (argc == 0):
+        punct = "."
+    print("{} {}{}".format(argc, msg, punct))
+    if (argc > 0):
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, av[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-av = sys.argv[:]
-ac = len(av) - 1
-psum = 0
-if (ac > 1):
-    for i in range(1, ac + 1):
-        psum += int(av[i])
-print("{}".format(psum))
+if (__name__ == "__main__"):
+    import sys
+    av = sys.argv[:]
+    ac = len(av) - 1
+    psum = 0
+    if (ac > 1):
+        for i in range(1, ac + 1):
+            psum += int(av[i])
+    print("{}".format(psum))
