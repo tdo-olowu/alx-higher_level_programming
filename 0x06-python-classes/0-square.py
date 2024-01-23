e0977609706b831445930562d0673be9291507c4
Tayo Olowu <olowu.otayo@gmail.com>
2024-01-23 22:29:56 +0100

    updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""is this what you want, checker?
+    ARE YOU HAPPY NOW?!!!!!!!!!
+    """
+
+
 class Square:
-    """A lazy do-nothing classs. Square.
+    """
+    A lazy do-nothing classs. Square.
     """
     pass
