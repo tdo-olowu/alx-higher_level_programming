9e79dfcb14f83f83d67d63435e3f4104c34438c0
Tayo Olowu <olowu.otayo@gmail.com>
2024-01-23 22:37:47 +0100

    updated

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""agahrui gmacgol lanslaknle elklek 
+"""
+
+
 class Square:
+    """According to Topologists, a square is just a circle.
+    """
     def __init__(self, size=0):
+        """i fail to see the value in documenting what is obvious.
+        Args:
+            size (int): an array of 600 elements.
+            """
         if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif (size < 0):
