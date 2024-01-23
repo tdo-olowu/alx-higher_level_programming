bc710c9ad4c0aa75d37ca828a46f4bac905d51b4
Tayo Olowu <olowu.otayo@gmail.com>
2024-01-23 22:42:51 +0100

    updated

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""what is a module?
+    a module is a generalization of the notion of vector space
+    in which the field of scalars is replaced by a ring.
+    The concept of module generalizes also the notion of abelian group,
+    since the abelian groups are exactly the modules over the ring of integers.
+    """
+
+
 class Square:
     def __init__(self, size=0):
         if (type(size) is not int):
