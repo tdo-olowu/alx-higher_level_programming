8d5b03911b55082941e79646f006ba8a6f581119
Tayo Olowu <olowu.otayo@gmail.com>
2024-01-23 21:52:27 +0100

    tedious

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
 
 class Square:
+    """The summary line for a class docstring should fit on one line.
+
+    public attributes, document here.
+    in an ``Attributes`` section follow same fmt as a
+    funct's ``Args`` section. Altly, attrs may be doced inline
+    inline w the attr's declaration (see __init__ method below).
+
+    Props created with the ``@property`` decorator should be documented
+    in the property's getter method.
+
+    Attributes:
+        attr1 (str): Description of `attr1`.
+        attr2 (:obj:`int`, optional): Description of `attr2`.
+
+    """
     pass
