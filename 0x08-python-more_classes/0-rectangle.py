d01850dec8dac8397e36c9e3a5cf52f9e69a0054
Tayo Olowu <olowu.otayo@gmail.com>
2024-01-29 20:39:11 +0100

    rectangle.py added

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""RECTANGLE: Explore object-oriented programming in Python via
+Rectangles, courtesy ALX
+"""
+
+
+class Rectangle:
+    """a simple class to model rectangles.
+    """
+    pass
