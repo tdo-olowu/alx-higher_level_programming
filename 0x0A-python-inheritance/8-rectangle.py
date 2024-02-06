33732935e8afb331531554c13ad593ef3792ec93
Tayo Olowu <olowu.otayo@gmail.com>
2024-02-06 01:02:23 +0100

    updated

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""the basis of all geometry, the base geometry
+will build everything else from here"""
+
+
+class BaseGeometry():
+    """BaseGeometry is a class which models
+    certain geometries"""
+
+    def area(self):
+        """the area of the geometry.
+        Return: area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """an integer validator which validates value
+        name is presumably always a string"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """a square which inherits from the base Geo"""
+
+
+    def __init__(self, width, height):
+        """the initialiser"""
+        super().__init__(self)
