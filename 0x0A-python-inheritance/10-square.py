90043c3cf35398bba634b75ae5392b8a47c2c0fd
Tayo Olowu <olowu.otayo@gmail.com>
2024-02-07 15:22:02 +0100

    updated

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""the basis of all geometry, the base geometry
+will build everything else from here"""
+
+
+Rect = __import__("9-rectangle").Rectangle
+
+
+class Square(Rect):
+    """a square which inherits from the base Geo"""
+    def __init__(self, size):
+        """the initialiser"""
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """implementing area"""
+        return (super().area())
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""the basis of all geometry, the base geometry
+will build everything else from here"""
+
+
+Rect = __import__("9-rectangle").Rectangle
+
+
+class Square(Rect):
+    """a square which inherits from the base Geo"""
+    def __init__(self, size):
+        """the initialiser"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """print implementation"""
+        width = self.__size
+        height = width
+        return ("[Rectangle] {}/{}".format(width, height))
+
+    def area(self):
+        """implementing area"""
+        return (self.__size ** 2)
