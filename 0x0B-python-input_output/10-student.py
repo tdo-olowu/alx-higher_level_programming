35e615645c6de4649190c3ccaf236dad09e492fb
Tayo Olowu <olowu.otayo@gmail.com>
2024-02-07 16:20:17 +0100

    updated

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -12,6 +12,21 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """dc"""
-        pass
+    def to_json(self, attrs = None):
+        """Expresses a class in serialisable form
+        Args:
+            attrs (any): should have serialisable attrs
+        Return:
+            the serialisable string of the object
+        """
+        if (attrs == None):
+            dct_repr = {"first_name": self.first_name, 
+                        "last_name": self.last_name,
+                        "age": self.age
+                        }
+        else:
+            dct_repr = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    dct_repr[attr] = getattr(self, attr)
+        return (dct_repr)
