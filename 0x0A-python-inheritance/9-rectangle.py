c09f96c88a3c37cc6cc88939d02047eed4399371
Tayo Olowu <olowu.otayo@gmail.com>
2024-02-07 15:14:14 +0100

    updated

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""the basis of all geometry, the base geometry
+will build everything else from here"""
+
+
+BGeo = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BGeo):
+    """a square which inherits from the base Geo"""
+    def __init__(self, width, height):
+        """the initialiser"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """print implementation"""
+        width = self.__width
+        height = self.__height
+        return ("[Rectangle] {}/{}".format(width, height))
+
+    def area(self):
+        """implementing area"""
+        return (self.__width * self.__height)
