f55cb852822144f27aaf8ed6ab5c56e6cf4077bc
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-13 03:18:10 +0100

    updated

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,7 @@
 -- displays scores >= 10
 -- lists scores from second_table
 -- score and name, ordered from best to worst
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
