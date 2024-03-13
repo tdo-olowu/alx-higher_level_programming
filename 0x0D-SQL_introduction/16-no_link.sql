9cb87edbe35dd1ce728ce47b3d03db3aa193f737
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-13 03:33:25 +0100

    updated

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -2,3 +2,12 @@
 -- does not list rows without a name value
 -- displays the score and the name (in that order)
 -- records listed by descending score
+SELECT
+	score,
+	name
+FROM
+	second_table
+WHERE
+	name IS NOT NULL
+ORDER BY
+	score DESC;
