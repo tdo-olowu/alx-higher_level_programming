4d823f5ffbf852e45b655fe51264e661c14db4e6
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-13 03:05:23 +0100

    updated

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- creates a second table in the database.
+-- id INT name VARCHAR score INT
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table (id, name, score)
+VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
