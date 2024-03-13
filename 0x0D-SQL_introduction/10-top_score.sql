b44cc035eca50613d7aa9c7a51bac726a51a9901
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-13 03:16:43 +0100

    updated

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,10 @@
 -- lists all records of second_table
 -- displays both score and name
 -- records ordered by descending order of score
-
+SELECT
+	score,
+	name
+FROM
+	second_table
+ORDER BY
+	score DESC;
