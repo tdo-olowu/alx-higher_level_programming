c9ea4fdb27dca923222811febb334bc8f7d46291
Tayo Olowu <olowu.otayo@gmail.com>
2024-03-13 03:31:29 +0100

    updated

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,11 @@
 -- lists the number of records with the same score in
-table second_table
+--	the second table
+SELECT
+	score,
+	COUNT(*) AS number
+FROM
+	second_table
+GROUP BY
+	score
+ORDER BY
+	number DESC;
