3378dd4a71d5168474b1e0c4da2242f2ef72e4af
Tayo Olowu <olowu.otayo@gmail.com>
2024-04-26 23:17:41 +0100

    updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+
+url = "https://alx-intranet.hbtn.io/status"
+rsvp = requests.get(url)
+msg = "Body response:\n"
+msg += "\t- type: {}\n".format(type(rsvp.text))
+msg += "\t- content: {}".format(rsvp.text)
+print(msg)
