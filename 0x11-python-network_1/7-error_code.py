87cb6131c4352aee404c8c54a03b36a89b813e35
Tayo Olowu <olowu.otayo@gmail.com>
2024-04-26 23:44:00 +0100

    updated

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""gets a resource using the requests module...
+but with exception handling"""
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    rsvp = requests.get(url)
+    stats = rsvp.status_code
+    if (stats >= 400):
+        # it's over 9000!
+        print("Error code: {}".format(stats))
+    else:
+        print(rsvp.text)
