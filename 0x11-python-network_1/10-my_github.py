35c667264b98f289f91d2393c1da361fc2e35d49
Tayo Olowu <olowu.otayo@gmail.com>
2024-04-27 00:04:39 +0100

    updated

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""accesses your github using requests"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    passw = sys.argv[2]
+
+    url = "https://api.github.com/user"
+    rsvp = requests.get(url, auth=(uname, passw))
+    if (rsvp.status_code == 200):
+        data = rsvp.json()
+        print(data["id"])
