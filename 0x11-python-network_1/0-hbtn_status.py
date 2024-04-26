c2933469cf7a0b9b54a5312e90a7cdd1343c9903
Tayo Olowu <olowu.otayo@gmail.com>
2024-04-26 21:59:20 +0100

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ link = "https://alx-intranet.hbtn.io/status"
 with urllib.request.urlopen(link) as rsvp:
     response = rsvp.read()
     msg = "Body response:\n"
-    msg += "\t-type: {}\n".format(type(response))
-    msg += "\t-content: {}\n".format(response)
-    msg += "\t-utf8 content: {}".format(response.decode("utf-8"))
+    msg += "\t- type: {}\n".format(type(response))
+    msg += "\t- content: {}\n".format(response)
+    msg += "\t- utf8 content: {}".format(response.decode("utf-8"))
     print(msg)
