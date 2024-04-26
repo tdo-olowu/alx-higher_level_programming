875bc13620438305d0ea34831748ea4330da1503
Tayo Olowu <olowu.otayo@gmail.com>
2024-04-26 23:31:40 +0100

    updated

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""uses requests to request url and display x-request-id"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    rsvp = requests.get(url)
+    xrid = rsvp.headers.get("X-Request-Id")
+    print(xrid)
