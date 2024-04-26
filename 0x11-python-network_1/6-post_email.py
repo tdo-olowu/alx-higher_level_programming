444d825e5ef12e94e9a85e6009c0ae545c5e257a
Tayo Olowu <olowu.otayo@gmail.com>
2024-04-26 23:38:27 +0100

    updated

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""posts to email using the requests module"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    params = {'email': email}
+    rsvp = requests.post(url, data=params)
+    print("Your email is: {}".format(rsvp.text))
